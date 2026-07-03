fedf1fbe627ee807afc8bcddd4d493abdb8b1517
mahender9390 <vennapureddymahender@gmail.com>
2026-07-03 13:55:37 +0530

    Initial Spring Boot setup

---
+++ backend/src/test/java/com/teamflow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamflow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
